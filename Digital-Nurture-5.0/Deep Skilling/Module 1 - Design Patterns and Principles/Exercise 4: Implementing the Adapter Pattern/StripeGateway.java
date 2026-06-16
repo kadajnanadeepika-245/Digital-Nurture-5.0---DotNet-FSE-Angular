b04367e0648f51b7c475810133074360a68a8f94
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-06-16 09:36:51 +0530

    Add StripeGateway class for payment processing

---
+++ Digital-Nurture-5.0/Deep Skilling/Module 1 - Design Patterns and Principles/Exercise 4: Implementing the Adapter Pattern/StripeGateway.java
@@ -0,0 +1,8 @@
+package adapter;
+
+public class StripeGateway {
+
+    public void pay(double amount) {
+        System.out.println("Payment of Rs." + amount + " processed through Stripe.");
+    }
+}
