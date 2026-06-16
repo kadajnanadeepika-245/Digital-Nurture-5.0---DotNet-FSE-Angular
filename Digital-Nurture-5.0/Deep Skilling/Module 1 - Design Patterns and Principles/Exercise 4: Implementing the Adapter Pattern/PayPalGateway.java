c66b6fd77dbe935c00bfe5168022bb2a8c47a9b5
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-06-16 09:36:30 +0530

    Update PayPalGateway.java

--- Digital-Nurture-5.0/Deep Skilling/Module 1 - Design Patterns and Principles/Exercise 4: Implementing the Adapter Pattern/PayPalGateway.java
+++ Digital-Nurture-5.0/Deep Skilling/Module 1 - Design Patterns and Principles/Exercise 4: Implementing the Adapter Pattern/PayPalGateway.java
@@ -1,1 +1,8 @@
+package adapter;
 
+public class PayPalGateway {
+
+    public void makePayment(double amount) {
+        System.out.println("Payment of Rs." + amount + " processed through PayPal.");
+    }
+}
