65509ed23885fe2e364b3807af3b785479f85973
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-06-16 09:38:11 +0530

    Create AdapterPatternTest.java

---
+++ Digital-Nurture-5.0/Deep Skilling/Module 1 - Design Patterns and Principles/Exercise 4: Implementing the Adapter Pattern/AdapterPatternTest.java
@@ -0,0 +1,16 @@
+package adapter;
+
+public class AdapterPatternTest {
+
+    public static void main(String[] args) {
+
+        PaymentProcessor paypal =
+                new PayPalAdapter(new PayPalGateway());
+
+        PaymentProcessor stripe =
+                new StripeAdapter(new StripeGateway());
+
+        paypal.processPayment(5000);
+        stripe.processPayment(7500);
+    }
+}
