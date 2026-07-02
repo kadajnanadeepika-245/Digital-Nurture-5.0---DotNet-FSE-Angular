75cad731a78f86b9187d27f2e427487936520df7
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-07-02 11:17:52 +0530

    Refactor PayPalGateway to C# syntax

--- Digital-Nurture-5.0/Deep Skilling/Module 1 - Design Patterns and Principles/Exercise 4: Implementing the Adapter Pattern/PayPalGateway.java
+++
@@ -1,8 +0,0 @@
-package adapter;
-
-public class PayPalGateway {
-
-    public void makePayment(double amount) {
-        System.out.println("Payment of Rs." + amount + " processed through PayPal.");
-    }
-}
