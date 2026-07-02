33f1219c2ec35716c3810bb149ea3df6c4528594
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-07-02 11:19:00 +0530

    Add StripeGateway.cs for Adapter Pattern implementation

--- Digital-Nurture-5.0/Deep Skilling/Module 1 - Design Patterns and Principles/Exercise 4: Implementing the Adapter Pattern/StripeGateway.java
+++
@@ -1,8 +0,0 @@
-package adapter;
-
-public class StripeGateway {
-
-    public void pay(double amount) {
-        System.out.println("Payment of Rs." + amount + " processed through Stripe.");
-    }
-}
