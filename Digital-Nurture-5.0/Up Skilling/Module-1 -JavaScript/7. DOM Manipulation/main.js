410710c6472b9e0a4aa38b6e1ccfb29ed4ca34f2
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-07-07 12:54:23 +0530

    Create main.js

---
+++ Digital-Nurture-5.0/Up Skilling/Module-1 -JavaScript/7. DOM Manipulation/main.js
@@ -0,0 +1,14 @@
+let container=
+document.querySelector("#events");
+
+
+let card=
+document.createElement("div");
+
+
+card.innerHTML=
+"Music Event <button>Register</button>";
+
+
+
+container.appendChild(card);
