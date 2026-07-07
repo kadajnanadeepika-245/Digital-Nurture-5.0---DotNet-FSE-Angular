754b234f76ecc978ed2c041bf051a6862061a6a5
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-07-07 20:08:31 +0530

    Create main.js

---
+++ Digital-Nurture-5.0/Up Skilling/Module-1 -JavaScript/14. jQuery and Frameworks/main.js
@@ -0,0 +1,15 @@
+$("#registerBtn").click(function(){
+
+alert("Registered");
+
+$(".eventCard").fadeOut();
+
+$(".eventCard").fadeIn();
+
+});
+
+
+
+console.log(
+"React and Vue provide reusable components and better state management"
+);
