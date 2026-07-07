858f2bb3617b855930fdf6ebc09682fe2b3cd79a
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-07-07 12:56:17 +0530

    Create main.js

---
+++ Digital-Nurture-5.0/Up Skilling/Module-1 -JavaScript/10. Modern JavaScript Features/main.js
@@ -0,0 +1,32 @@
+const event={
+
+name:"Music",
+
+location:"City Hall"
+
+};
+
+
+
+let {name,location}=event;
+
+
+
+console.log(name);
+
+
+
+function display(msg="Welcome"){
+
+console.log(msg);
+
+}
+
+
+
+
+let copy={...event};
+
+
+
+console.log(copy);
