59968bf693651e01b1429ab49a29762934217686
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-07-07 12:58:15 +0530

    Create main.js

---
+++ Digital-Nurture-5.0/Up Skilling/Module-1 -JavaScript/13. Debugging and Testing/main.js
@@ -0,0 +1,24 @@
+console.log("Form Submit Started");
+
+
+let username="Deepika";
+
+
+debugger;
+
+
+console.log(username);
+
+
+
+/*
+
+Chrome DevTools:
+
+1. Inspect
+2. Console - check logs
+3. Network - check API
+4. Sources - add breakpoint
+5. Watch variables
+
+*/
