37f3fde9f86000d7a89d118b7d0ba81223b96219
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-07-24 16:37:58 +0530

    Create Calculator.cs

---
+++ Digital-Nurture-5.0/Deep Skilling/Module-14-GenAI-Fundamentals/Sample_Code/Calculator.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Calculator
+{
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(Add(10,20));
+    }
+}
