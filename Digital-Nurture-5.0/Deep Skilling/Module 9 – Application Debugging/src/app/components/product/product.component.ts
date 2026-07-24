c09246e644b401c0a00cc1e5d6b3f632882df449
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-07-24 15:30:00 +0530

    Add ProductComponent for product management

---
+++ Digital-Nurture-5.0/Deep Skilling/Module 9 – Application Debugging/src/app/components/product/product.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+
+  selector: 'app-product',
+
+  templateUrl: './product.component.html',
+
+  styleUrls: ['./product.component.css']
+
+})
+
+export class ProductComponent implements OnInit {
+
+  products: Product[] = [];
+
+  selectedProduct: Product | null = null;
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+
+    this.products = this.productService.getProducts();
+
+    console.log("Products Loaded");
+
+  }
+
+  selectProduct(product: Product): void {
+
+    this.selectedProduct = product;
+
+  }
+
+}
