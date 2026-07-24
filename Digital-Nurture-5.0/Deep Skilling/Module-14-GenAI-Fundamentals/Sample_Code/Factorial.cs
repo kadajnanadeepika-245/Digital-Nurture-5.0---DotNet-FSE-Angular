d22de5e12db3a018cfb3022c3daa154d21e095fb
Kada Jnana Deepika Lakshmi Sowmya <jnanakadaepdcl@gmail.com>
2026-07-24 16:38:43 +0530

    Create Factorial.cs

---
+++ Digital-Nurture-5.0/Deep Skilling/Module-14-GenAI-Fundamentals/Sample_Code/Factorial.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Factorial
+{
+    static int Fact(int n)
+    {
+        if(n<=1)
+            return 1;
+
+        return n*Fact(n-1);
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(Fact(5));
+    }
+}
